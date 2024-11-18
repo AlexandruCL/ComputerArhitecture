23da5c7cfe2b8963f99f6991b2ca2d2a475924f1
Alexandru CL <laur.cristea@student.upt.ro>
2024-11-18 11:27:43 +0200

    Update problem84.v

--- Lab8/problem84.v
+++ Lab8/problem84.v
@@ -1,5 +1,12 @@
 module bist(
-    input clk, rst,
-    output [3:0] q
-);
-
+  input clk, rst,set,
+  output reg [0:3]q);
+  
+  wire [0:3]w1;
+  wire w2;
+  
+  lfsr_4b tpg(.clk(clk), .set(set), rst(1'b1), .q(w1));
+  checker dut(.i(w1), .o(w2));
+  sisr ora(.i(w2), .clk(clk), .rst(rst), .q(q), .set(1'b1));
+  
+endmodule;
